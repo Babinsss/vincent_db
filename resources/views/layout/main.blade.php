264cd16ece475b6a84465fc2be77ca50a80d7c7f
Binns <vincentbarrientos51@gmail.com>
2024-03-05 11:53:36 +0800

    intl commit

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,10 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" </head>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+</head>
 
 <body>
     @yield('content')
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index']);
-
 Route::get('/users', function () {
     return view('user.index');
 });
+
+// Route for storing user
+Route::post('/user/store', [UserController::class, 'store'])->name('user.store');
