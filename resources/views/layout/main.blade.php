46e897854216d434ce0f579cf7489011ed27d7ef
Rvondc <rogivon.delacruz@gmail.com>
2024-04-02 21:35:52 +0800

    modify main layout

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+
+    <title>@yield('title')</title>
 </head>
+
 <body>
-        <div class="content">
-            @yield('content')
+    <div class="content">
+        @yield('content')
     </div>
     <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
 </body>
+
 </html>
