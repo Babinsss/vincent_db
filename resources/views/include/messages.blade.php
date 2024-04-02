9f0327e7da6d4436afca9f46ed297193ef2785c9
Rvondc <rogivon.delacruz@gmail.com>
2024-04-02 21:35:33 +0800

    modified messages

--- resources/views/include/messages.blade.php
+++ resources/views/include/messages.blade.php
@@ -1,5 +1,5 @@
 @if (session()->has('message_success'))
     <div class="alert alert-success" role="alert">
-        {{ session('message') }}
+        {{ session('message_success') }}
     </div>
 @endif
