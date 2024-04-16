76b376ca5b77e2dfd2d87aeffe7e76f923b77274
Babinsss <vincentbarrientos51@gmail.com>
2024-04-16 10:49:49 +0800

    database User_image added

--- database/migrations/2024_03_08_030652_create_table_users.php
+++ database/migrations/2024_03_08_030652_create_table_users.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use League\CommonMark\Extension\Table\Table;
 
 return new class extends Migration
 {
@@ -15,6 +16,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id('user_id');
+            $table->string('user_image',255)->nullable();
             $table->string('first_name', 55);
             $table->string('middle_name', 55)->nullable();
             $table->string('last_name', 55);
